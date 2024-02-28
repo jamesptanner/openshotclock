2e5c8f924f26dbcfc2009ccadf03c378f2ac36a3
James Tanner <jamesptanner@gmail.com>
2024-02-28 23:00:22 +0000

    Set Defines for breadboard version of shotclock.

--- src/openshotclock.cpp
+++ src/openshotclock.cpp
@@ -1,13 +1,21 @@
 #include "max7219.h"
 #include "pico/stdlib.h"
 #include "stdio.h"
+
+#define MAX_LOAD 6
+#define MAX_DIN 13
+#define MAX_CLK 12
+#define CTRL_ADD 8
+#define CTRL_RST 10
+#define CTRL_STR_STOP 11 
+
 int main(){
   stdio_init_all();
 
   //load -> gpio6 -> pin9
   //clock -> gpio12 -> pin16
   //data in -> gpio13 -> pin16
-  configureMax7219(13,6,12);
+  configureMax7219(MAX_DIN,MAX_LOAD,MAX_CLK);
   setShutdown(false);
   setNumOfDigits(2);
   setDecodeMode(0xff);
