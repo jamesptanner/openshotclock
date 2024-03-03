a5cc7fa47baa781ab494daacf5e002d9a3e54db6
James Tanner <jamesptanner@outlook.com>
2024-03-03 12:10:10 +0000

    Begin preparing the countdown

---
+++ src/clock.h
@@ -0,0 +1,6 @@
+#include "pico/stdlib.h"
+
+void addSecondToTimer(unsigned int, long unsigned int);
+void resetTimer(unsigned int, long unsigned int);
+void startStopTimer(unsigned int, long unsigned int);
+uint8_t getSecondsLeft();
--- src/openshotclock.cpp
+++ src/openshotclock.cpp
@@ -1,4 +1,6 @@
 #include "max7219.h"
+#include "clock.h"
+
 #include "pico/stdlib.h"
 #include "stdio.h"
 
@@ -9,12 +11,10 @@
 #define CTRL_RST 10
 #define CTRL_STR_STOP 11 
 
-int main(){
-  stdio_init_all();
-
+void setupCounter(){
   //load -> gpio6 -> pin9
   //clock -> gpio12 -> pin16
-  //data in -> gpio13 -> pin16
+  //data in -> gpio13 -> pin17
   configureMax7219(MAX_DIN,MAX_LOAD,MAX_CLK);
   setShutdown(true);
   setNumOfDigits(2);
@@ -23,11 +23,26 @@ int main(){
   setBrightness(1);
   setShutdown(false);
   setTestMode(false);
+}
+
+void setupTimer(){
+  gpio_set_irq_enabled_with_callback(CTRL_ADD, GPIO_IRQ_EDGE_RISE, true, &addSecondToTimer);
+  gpio_set_irq_enabled_with_callback(CTRL_RST, GPIO_IRQ_EDGE_RISE, true, &resetTimer);
+  gpio_set_irq_enabled_with_callback(CTRL_STR_STOP, GPIO_IRQ_EDGE_RISE, true, &startStopTimer);
+}
+
+int main(){
+  stdio_init_all();
+
+  setupCounter();
+  setupTimer();
+
   uint8_t num = 0;
   while (true){ 
-    sleep_ms(1000);
+    num = getSecondsLeft();
     setNumber(num);
-    num++;
+    sleep_ms(100);
+
   }
   return 0;
 }
